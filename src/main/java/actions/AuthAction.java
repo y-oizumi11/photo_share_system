3a1b2bba01daa0ce594d4e61f25e0060670feff1
yukari.oizumi <y.oizumi95@gmail.com>
2023-06-20 11:38:35 +0900

    AuthAction変更

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import actions.views.UserView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.MessageConst;
+import constants.PropertyConst;
 import services.UserService;
 
 public class AuthAction extends ActionBase {
@@ -26,6 +29,35 @@ public class AuthAction extends ActionBase {
             removeSessionScope(AttributeConst.FLUSH);
         }
         forward(ForwardConst.FW_LOGIN);
+    }
+
+    public void login()throws ServletException, IOException{
+        String code = getRequestParam(AttributeConst.U_CODE);
+        String plainPass = getRequestParam(AttributeConst.U_PASS);
+        String pepper = getContextScope(PropertyConst.PEPPER);
+
+        Boolean isValidUser = service.validateLogin(code, plainPass, pepper);
+        
+        if(isValidUser) {
+            if(checkToken()) {
+                UserView uv = service.findOne(code, plainPass, pepper);
+                putSessionScope(AttributeConst.LOGIN_USER, uv);
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGINED.getMessage());
+
+                redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
+            }
+        }else {
+            putRequestScope(AttributeConst.TOKEN, getTokenId());
+            putRequestScope(AttributeConst.LOGIN_ERR, true);
+            putRequestScope(AttributeConst.U_CODE, code);
+            forward(ForwardConst.FW_LOGIN);
+        }
+    }
+
+    public void logout() throws ServletException, IOException{
+        removeSessionScope(AttributeConst.LOGIN_USER);
+        putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGOUT.getMessage());
+        redirect(ForwardConst.ACT_AUTH, ForwardConst.CMD_SHOW_LOGIN);
     }
 
 }
