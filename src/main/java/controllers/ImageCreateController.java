3ead36d9f673cd708f2ab5a64e701d0e842be7d4
yukari.oizumi <y.oizumi95@gmail.com>
2023-06-24 17:28:33 +0900

    imageCreateController修正

--- src/main/java/controllers/ImageCreateController.java
+++ src/main/java/controllers/ImageCreateController.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import java.io.File;
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -12,43 +14,40 @@ import javax.servlet.http.Part;
 
 import constants.AttributeConst;
 
-/**
- * Servlet implementation class ImageCreateController
- */
+
 @WebServlet("/ImageCreateController")
 @MultipartConfig
 
 public class ImageCreateController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ImageCreateController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/images/_form.jsp");
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
+        rd.forward(request, response);
+
+   }
+
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
+
 
         String imgAdd = request.getParameter(AttributeConst.IMG_ADDRESS.getValue());
 
         Part part = request.getPart(imgAdd);
-        String filePath = "/upload/" + part.getSubmittedFileName();
-        part.write(getServletContext().getRealPath(filePath));
-        // System.out.println(getServletContext().getRealPath(filePath));
+
+        String fileName = part.getSubmittedFileName();
+
+        String path = getServletContext().getRealPath("/upload");
+        System.out.println(path);
+
+        part.write(path + File.separator + fileName);
+
 
         doGet(request, response);
     }
